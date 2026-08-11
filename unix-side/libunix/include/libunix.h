970ecc71209078542e7497359f537740add67071
carrolos <262498693+carrolos@users.noreply.github.com>
2026-08-11 12:41:29 -0700

    initial scaffolding and Makefile for unix side. as it stands, a simple hello world file compiles while linking a library created by archiving unix-side/libunix

---
+++ unix-side/libunix/include/libunix.h
@@ -0,0 +1,10 @@
+#ifndef __LIBUNIX_H__
+#define __LIBUNIX_H__
+
+#include <string.h>
+#include <stdbool.h>
+
+int suffix_cmp(const char *s, const char *suffix);
+int prefix_cmp(const char *s, const char *prefix);
+
+#endif
---
+++ unix-side/rpz-install.c
@@ -0,0 +1,10 @@
+// inspired by Dawson Engler's bootloader for CS140E loads a program to a rpi
+// zero connected via a tty-USB device
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world!\n");
+    return 0;
+}
