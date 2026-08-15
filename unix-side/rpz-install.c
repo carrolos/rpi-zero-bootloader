492ba34371d34f4abcb5129995728a96e4bc079f
carrolos <262498693+carrolos@users.noreply.github.com>
2026-08-14 19:40:07 -0700

    started actually writing unix side bootloader. handled basic skeleton for options. need to finish checking bogus options

--- unix-side/rpz-install.c
+++ unix-side/rpz-install.c
@@ -2,17 +2,88 @@
 // zero connected via a tty-USB device
 
 #include <stdio.h>
+#include <getopt.h>
 
 #include "libunix.h"
 
-int main(void)
+static int f_verbose;
+
+static struct option long_options[] =
 {
-    char* my_str = "affix";
-    printf("str is %s\n", my_str);
-    printf("prefix of str is af? %s\n", 
-        prefix_cmp(my_str, "af") ? "true" : "false");
-    printf("suffix of str is fix? %s\n", 
-        suffix_cmp(my_str, "fix") ? "true" : "false");
+    {"baud-rate", required_argument, NULL,     'b'},
+    {"last",      no_argument,       NULL,     'l'},
+    {"first",     no_argument,       NULL,     'f'},
+    {"device",    required_argument, NULL,     'd'},
+    {"verbose",   no_argument,       &f_verbose, 1},
+    // sentinel
+    {NULL,        0,                 NULL,      0 }
+};
+
+// MUST TODO: add a usage function that prints how to use the bootloader
+
+
+int main(int argc, char ** argv)
+{
+    char * this_prog = argv[0];
+    uint32_t baud = B115200;
+    char * dev_name = "";
+    char * pi_prog = "";
+
+    int opt_index;
+    int c;
+    while (1)
+    {
+        // the leading '-' tells getopt to not reorder argv
+        // the ':' after the leading '-' tells getopt that we'll handle errors
+        c = getopt_long(argc, argv, "-:b:lfd:v", long_options, &opt_index); 
+        if (c == -1)
+            break;
+        switch (c)
+        {
+            // TODO: handle more than one non-opt arg; can only bootload one
+            // prog!!
+            case 1:
+                printf("got prog <%s>\n", optarg);
+                pi_prog = optarg;
+                break;
+            // TODO: parse baud rate; only allow those defined in termios.h
+            case 'b':
+                printf("changing baud rate to <%s>\n", optarg);
+                break;
+            case 'l':
+                printf("will look for last device\n");
+                break;
+            case 'f':
+                printf("will look for first device\n");
+                break;
+            // bogus device handled by find_tty
+            case 'd':
+                printf("will look for device <%s>\n", optarg);
+                break;
+            // consider editing array entry because decoupled; seems simple
+            // enough to not cause problems, however
+            case 'v':
+                f_verbose = 1;
+            case '?':
+                fprintf(stderr, "Unknown option <%c>\n", optopt);
+                break;
+            case ':':
+                fprintf(stderr, "Missing argument for option <%c>\n", optopt);
+                break;
+            default:
+                fprintf(stderr, "??? getopt returned character code <%c>\n",
+                    c);
+                break;
+        }
+    }
+    if (f_verbose)
+    {
+        printf("verbose mode activated\n");
+        printf("bootloader settings:\n");
+        printf("\t baud-rate: %u\n", baud);
+        printf("\t    device: %s\n", dev_name);
+        printf("\tpi-program: %s\n", pi_prog);
+    }
 
     return 0;
 }
